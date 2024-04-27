894c65b970929379caaef976e9c5793d1370cf89
vaishakh2510 <vaishakhb2510@gmail.com>
2024-04-27 15:14:03 +0530

    first commit

---
+++ src/main/java/com/example/ProductServiceJPA/ProductServiceJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductServiceJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProductServiceJPA/ProductServiceJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductServiceJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
